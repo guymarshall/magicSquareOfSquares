d9ea62ec07ab0adf07aac8184e647a7d36bc4022
Guy Marshall <guymarshall9@gmail.com>
2023-06-22 20:07:26 +0100

    Add struct MagicSquare

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+struct MagicSquare {
+    number1: i32,
+    number2: i32,
+    number3: i32,
+    number4: i32,
+    number5: i32,
+    number6: i32,
+    number7: i32,
+    number8: i32,
+    number9: i32
+}
+
 fn main() {
     println!("Hello, world!");
 }
