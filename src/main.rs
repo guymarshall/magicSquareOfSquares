6fe9d301f986d777a88c7aa0ed203cd2c6233bb6
Guy Marshall <guymarshall9@gmail.com>
2023-06-22 20:10:25 +0100

    Move MagicSquare code to magic_square.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-struct MagicSquare {
-    number1: i32,
-    number2: i32,
-    number3: i32,
-    number4: i32,
-    number5: i32,
-    number6: i32,
-    number7: i32,
-    number8: i32,
-    number9: i32
-}
-
-impl MagicSquare {
-    fn new(number1: i32, number2: i32, number3: i32, number4: i32, number5: i32, number6: i32, number7: i32, number8: i32, number9: i32) -> MagicSquare {
-        MagicSquare {
-            number1,
-            number2,
-            number3,
-            number4,
-            number5,
-            number6,
-            number7,
-            number8,
-            number9
-        }
-    }
-}
-
 fn main() {
     println!("Hello, world!");
 }
