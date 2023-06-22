456373064099551d65b040af876ebaee3f4ab2a3
Guy Marshall <guymarshall9@gmail.com>
2023-06-22 20:31:44 +0100

    Change new() to save squared numbers

--- src/magic_square.rs
+++ src/magic_square.rs
@@ -13,15 +13,15 @@ pub struct MagicSquare {
 impl MagicSquare {
     pub fn new(number1: i32, number2: i32, number3: i32, number4: i32, number5: i32, number6: i32, number7: i32, number8: i32, number9: i32) -> MagicSquare {
         MagicSquare {
-            number1,
-            number2,
-            number3,
-            number4,
-            number5,
-            number6,
-            number7,
-            number8,
-            number9
+            number1: number1 * number1,
+            number2: number2 * number2,
+            number3: number3 * number3,
+            number4: number4 * number4,
+            number5: number5 * number5,
+            number6: number6 * number6,
+            number7: number7 * number7,
+            number8: number8 * number8,
+            number9: number9 * number9
         }
     }
 }
