d039d2c358a4fd6b80a66e687b1cac49a5902a9b
Guy Marshall <guymarshall9@gmail.com>
2023-06-25 10:16:53 +0100

    Add into_par_iter() for increased performance

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::magic_square::MagicSquare;
+use rayon::prelude::*;
 
 mod magic_square;
 
@@ -20,7 +21,7 @@ fn numbers_are_unique(number1: i32, number2: i32, number3: i32, number4: i32, nu
 fn main() {
     const LIMIT: i32 = 8;
 
-    (0..LIMIT).for_each(|a| {
+    (0..LIMIT).into_par_iter().for_each(|a| {
         (0..LIMIT).for_each(|b| {
             (0..LIMIT).for_each(|c| {
                 (0..LIMIT).for_each(|d| {
