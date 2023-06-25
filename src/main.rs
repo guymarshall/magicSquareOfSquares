ec3d4cbfe4e380aefc4f8b065ff7cab801217417
Guy Marshall <guymarshall9@gmail.com>
2023-06-25 10:36:35 +0100

    Replace numbers_are_unique() with branchless version for performance

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,9 @@ use rayon::prelude::*;
 mod magic_square;
 
 fn numbers_are_unique(number1: i32, number2: i32, number3: i32, number4: i32, number5: i32, number6: i32, number7: i32, number8: i32, number9: i32) -> bool {
-    number1 != number2 && number1 != number3 && number1 != number4 && number1 != number5 && number1 != number6 && number1 != number7 && number1 != number8 && number1 != number9 &&
-    number2 != number3 && number2 != number4 && number2 != number5 && number2 != number6 && number2 != number7 && number2 != number8 && number2 != number9 &&
-    number3 != number4 && number3 != number5 && number3 != number6 && number3 != number7 && number3 != number8 && number3 != number9 &&
-    number4 != number5 && number4 != number6 && number4 != number7 && number4 != number8 && number4 != number9 &&
-    number5 != number6 && number5 != number7 && number5 != number8 && number5 != number9 &&
-    number6 != number7 && number6 != number8 && number6 != number9 &&
-    number7 != number8 && number7 != number9 &&
-    number8 != number9
+    let mask: u8 = ((1 << number1) | (1 << number2) | (1 << number3) | (1 << number4) | (1 << number5) | (1 << number6) | (1 << number7) | (1 << number8) | (1 << number9));
+    let count: u32 = mask.count_ones();
+    count == 9
 }
 
 fn main() {
