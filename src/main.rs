4800aa47114004d8ff52affb5483ad3e5c29a234
Guy Marshall <guymarshall9@gmail.com>
2023-06-28 20:40:44 +0100

    Add sums_are_equal() to main.rs separate from MagicSquare struct

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,29 @@ fn numbers_are_unique(number1: i32, number2: i32, number3: i32, number4: i32, nu
         || number8 == number9)
 }
 
+fn sums_are_equal(numbers: [i32; 9]) -> bool {
+    let top_row_sum = numbers[0] + numbers[1] + numbers[2];
+    let middle_row_sum = numbers[3] + numbers[4] + numbers[5];
+    let bottom_row_sum = numbers[6] + numbers[7] + numbers[8];
+
+    if top_row_sum != middle_row_sum || middle_row_sum != bottom_row_sum {
+        return false;
+    }
+
+    let left_column_sum = numbers[0] + numbers[3] + numbers[6];
+    let middle_column_sum = numbers[1] + numbers[4] + numbers[7];
+    let right_column_sum = numbers[2] + numbers[5] + numbers[8];
+
+    if bottom_row_sum != left_column_sum || left_column_sum != middle_column_sum || middle_column_sum != right_column_sum {
+        return false;
+    }
+
+    let nw_se_sum = numbers[0] + numbers[4] + numbers[8];
+    let sw_ne_sum = numbers[6] + numbers[4] + numbers[2];
+
+    right_column_sum == nw_se_sum && nw_se_sum == sw_ne_sum
+}
+
 fn main() {
     const LIMIT: i32 = 40;
 
