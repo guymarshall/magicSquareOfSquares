e42e574d9a5ed4edae1e4341a58e4727f86646e9
Guy Marshall <guymarshall9@gmail.com>
2023-06-28 20:53:56 +0100

    Total rework of magic_square.rs - now uses array of i32

--- src/main.rs
+++ src/main.rs
@@ -3,46 +3,46 @@ use rayon::prelude::*;
 
 mod magic_square;
 
-fn numbers_are_unique(number1: i32, number2: i32, number3: i32, number4: i32, number5: i32, number6: i32, number7: i32, number8: i32, number9: i32) -> bool {
-    !(number1 == number2
-        || number1 == number3
-        || number1 == number4
-        || number1 == number5
-        || number1 == number6
-        || number1 == number7
-        || number1 == number8
-        || number1 == number9
-        || number2 == number3
-        || number2 == number4
-        || number2 == number5
-        || number2 == number6
-        || number2 == number7
-        || number2 == number8
-        || number2 == number9
-        || number3 == number4
-        || number3 == number5
-        || number3 == number6
-        || number3 == number7
-        || number3 == number8
-        || number3 == number9
-        || number4 == number5
-        || number4 == number6
-        || number4 == number7
-        || number4 == number8
-        || number4 == number9
-        || number5 == number6
-        || number5 == number7
-        || number5 == number8
-        || number5 == number9
-        || number6 == number7
-        || number6 == number8
-        || number6 == number9
-        || number7 == number8
-        || number7 == number9
-        || number8 == number9)
+fn numbers_are_unique(numbers: &[i32; 9]) -> bool {
+    !(numbers[0] == numbers[1]
+        || numbers[0] == numbers[2]
+        || numbers[0] == numbers[3]
+        || numbers[0] == numbers[4]
+        || numbers[0] == numbers[5]
+        || numbers[0] == numbers[6]
+        || numbers[0] == numbers[7]
+        || numbers[0] == numbers[8]
+        || numbers[1] == numbers[2]
+        || numbers[1] == numbers[3]
+        || numbers[1] == numbers[4]
+        || numbers[1] == numbers[5]
+        || numbers[1] == numbers[6]
+        || numbers[1] == numbers[7]
+        || numbers[1] == numbers[8]
+        || numbers[2] == numbers[3]
+        || numbers[2] == numbers[4]
+        || numbers[2] == numbers[5]
+        || numbers[2] == numbers[6]
+        || numbers[2] == numbers[7]
+        || numbers[2] == numbers[8]
+        || numbers[3] == numbers[4]
+        || numbers[3] == numbers[5]
+        || numbers[3] == numbers[6]
+        || numbers[3] == numbers[7]
+        || numbers[3] == numbers[8]
+        || numbers[4] == numbers[5]
+        || numbers[4] == numbers[6]
+        || numbers[4] == numbers[7]
+        || numbers[4] == numbers[8]
+        || numbers[5] == numbers[6]
+        || numbers[5] == numbers[7]
+        || numbers[5] == numbers[8]
+        || numbers[6] == numbers[7]
+        || numbers[6] == numbers[8]
+        || numbers[7] == numbers[8])
 }
 
-fn sums_are_equal(numbers: [i32; 9]) -> bool {
+fn sums_are_equal(numbers: &[i32; 9]) -> bool {
     let top_row_sum: i32 = numbers[0] + numbers[1] + numbers[2];
     let middle_row_sum: i32 = numbers[3] + numbers[4] + numbers[5];
     let bottom_row_sum: i32 = numbers[6] + numbers[7] + numbers[8];
@@ -68,6 +68,8 @@ fn sums_are_equal(numbers: [i32; 9]) -> bool {
 fn main() {
     const LIMIT: i32 = 40;
 
+    let square: MagicSquare = MagicSquare::new([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
     (0..LIMIT).for_each(|a| {
         (0..LIMIT).into_par_iter().for_each(|b| {
             (0..LIMIT).for_each(|c| {
@@ -77,11 +79,11 @@ fn main() {
                             (0..LIMIT).for_each(|g| {
                                 (0..LIMIT).for_each(|h| {
                                     (0..LIMIT).for_each(|i| {
-                                        let numbers_are_unique: bool = numbers_are_unique(a, b, c, d, e, f, g, h, i);
+                                        let numbers_are_unique: bool = numbers_are_unique(&square.numbers);
 
                                         if numbers_are_unique {
-                                            let square: MagicSquare = MagicSquare::new(a, b, c, d, e, f, g, h, i);
-                                            let is_magic: bool = square.sums_are_equal();
+                                            square.clone().set([a, b, c, d, e, f, g, h, i]);
+                                            let is_magic: bool = sums_are_equal(&square.numbers);
 
                                             if is_magic {
                                                 println!("{:?}", square);
