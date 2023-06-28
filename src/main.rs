1a46bb1aafd562269cb64cfd88b5fbc3619d18dd
Guy Marshall <guymarshall9@gmail.com>
2023-06-28 17:25:02 +0100

    Change exclusive range to inclusive range

--- src/main.rs
+++ src/main.rs
@@ -45,15 +45,15 @@ fn numbers_are_unique(number1: i32, number2: i32, number3: i32, number4: i32, nu
 fn main() {
     const LIMIT: i32 = 40;
 
-    (0..LIMIT).into_par_iter().for_each(|a| {
-        (0..LIMIT).for_each(|b| {
-            (0..LIMIT).for_each(|c| {
-                (0..LIMIT).for_each(|d| {
-                    (0..LIMIT).for_each(|e| {
-                        (0..LIMIT).for_each(|f| {
-                            (0..LIMIT).for_each(|g| {
-                                (0..LIMIT).for_each(|h| {
-                                    (0..LIMIT).for_each(|i| {
+    (0..=LIMIT).into_par_iter().for_each(|a| {
+        (0..=LIMIT).for_each(|b| {
+            (0..=LIMIT).for_each(|c| {
+                (0..=LIMIT).for_each(|d| {
+                    (0..=LIMIT).for_each(|e| {
+                        (0..=LIMIT).for_each(|f| {
+                            (0..=LIMIT).for_each(|g| {
+                                (0..=LIMIT).for_each(|h| {
+                                    (0..=LIMIT).for_each(|i| {
                                         let numbers_are_unique: bool = numbers_are_unique(a, b, c, d, e, f, g, h, i);
 
                                         if numbers_are_unique {
