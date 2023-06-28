39fc1998e240479a1ca0fe703a8a3d5a2df6e552
Guy Marshall <guymarshall9@gmail.com>
2023-06-28 21:08:22 +0100

    2nd attempt at committing: Total rework of magic_square.rs - now uses array of i32

--- src/magic_square.rs
+++ src/magic_square.rs
@@ -1,62 +1,59 @@
 #[derive(Debug)]
+#[derive(Clone)]
 pub struct MagicSquare {
-    number1: i32,
-    number2: i32,
-    number3: i32,
-    number4: i32,
-    number5: i32,
-    number6: i32,
-    number7: i32,
-    number8: i32,
-    number9: i32
+    pub(crate) numbers: [i32; 9]
 }
 
 impl MagicSquare {
-    pub fn new(number1: i32, number2: i32, number3: i32, number4: i32, number5: i32, number6: i32, number7: i32, number8: i32, number9: i32) -> MagicSquare {
+    pub fn new(numbers: [i32; 9]) -> MagicSquare {
         MagicSquare {
-            number1: number1 * number1,
-            number2: number2 * number2,
-            number3: number3 * number3,
-            number4: number4 * number4,
-            number5: number5 * number5,
-            number6: number6 * number6,
-            number7: number7 * number7,
-            number8: number8 * number8,
-            number9: number9 * number9
+            numbers: [
+                numbers[0] * numbers[0],
+                numbers[1] * numbers[1],
+                numbers[2] * numbers[2],
+                numbers[3] * numbers[3],
+                numbers[4] * numbers[4],
+                numbers[5] * numbers[5],
+                numbers[6] * numbers[6],
+                numbers[7] * numbers[7],
+                numbers[8] * numbers[8]
+            ]
         }
     }
 
-    pub fn set(&mut self, number1: i32, number2: i32, number3: i32, number4: i32, number5: i32, number6: i32, number7: i32, number8: i32, number9: i32) {
-        self.number1 = number1 * number1;
-        self.number2 = number2 * number2;
-        self.number3 = number3 * number3;
-        self.number4 = number4 * number4;
-        self.number5 = number5 * number5;
-        self.number6 = number6 * number6;
-        self.number7 = number7 * number7;
-        self.number8 = number8 * number8;
-        self.number9 = number9 * number9;
+    pub fn set(&mut self, numbers: [i32; 9]) {
+        self.numbers = [
+            numbers[0] * numbers[0],
+            numbers[1] * numbers[1],
+            numbers[2] * numbers[2],
+            numbers[3] * numbers[3],
+            numbers[4] * numbers[4],
+            numbers[5] * numbers[5],
+            numbers[6] * numbers[6],
+            numbers[7] * numbers[7],
+            numbers[8] * numbers[8]
+        ]
     }
 
     pub fn sums_are_equal(&self) -> bool {
-        let top_row_sum = self.number1 + self.number2 + self.number3;
-        let middle_row_sum = self.number4 + self.number5 + self.number6;
-        let bottom_row_sum = self.number7 + self.number8 + self.number9;
+        let top_row_sum: i32 = self.numbers[0] + self.numbers[1] + self.numbers[2];
+        let middle_row_sum: i32 = self.numbers[3] + self.numbers[4] + self.numbers[5];
+        let bottom_row_sum: i32 = self.numbers[6] + self.numbers[7] + self.numbers[8];
 
         if top_row_sum != middle_row_sum || middle_row_sum != bottom_row_sum {
             return false;
         }
 
-        let left_column_sum = self.number1 + self.number4 + self.number7;
-        let middle_column_sum = self.number2 + self.number5 + self.number8;
-        let right_column_sum = self.number3 + self.number6 + self.number9;
+        let left_column_sum: i32 = self.numbers[0] + self.numbers[3] + self.numbers[6];
+        let middle_column_sum: i32 = self.numbers[1] + self.numbers[4] + self.numbers[7];
+        let right_column_sum: i32 = self.numbers[2] + self.numbers[5] + self.numbers[8];
 
         if bottom_row_sum != left_column_sum || left_column_sum != middle_column_sum || middle_column_sum != right_column_sum {
             return false;
         }
 
-        let nw_se_sum = self.number1 + self.number5 + self.number9;
-        let sw_ne_sum = self.number7 + self.number5 + self.number3;
+        let nw_se_sum: i32 = self.numbers[0] + self.numbers[4] + self.numbers[8];
+        let sw_ne_sum: i32 = self.numbers[6] + self.numbers[4] + self.numbers[2];
 
         right_column_sum == nw_se_sum && nw_se_sum == sw_ne_sum
     }
