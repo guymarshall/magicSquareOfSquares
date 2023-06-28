4d43ac26a48b2753481f75a164a07a93627dd8f6
Guy Marshall <guymarshall9@gmail.com>
2023-06-28 21:28:12 +0100

    Change arrays to i32 numbers again

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,63 @@
 use rayon::prelude::*;
 
-fn numbers_are_unique(numbers: [i32; 9]) -> bool {
-    !(numbers[0] == numbers[1]
-        || numbers[0] == numbers[2]
-        || numbers[0] == numbers[3]
-        || numbers[0] == numbers[4]
-        || numbers[0] == numbers[5]
-        || numbers[0] == numbers[6]
-        || numbers[0] == numbers[7]
-        || numbers[0] == numbers[8]
-        || numbers[1] == numbers[2]
-        || numbers[1] == numbers[3]
-        || numbers[1] == numbers[4]
-        || numbers[1] == numbers[5]
-        || numbers[1] == numbers[6]
-        || numbers[1] == numbers[7]
-        || numbers[1] == numbers[8]
-        || numbers[2] == numbers[3]
-        || numbers[2] == numbers[4]
-        || numbers[2] == numbers[5]
-        || numbers[2] == numbers[6]
-        || numbers[2] == numbers[7]
-        || numbers[2] == numbers[8]
-        || numbers[3] == numbers[4]
-        || numbers[3] == numbers[5]
-        || numbers[3] == numbers[6]
-        || numbers[3] == numbers[7]
-        || numbers[3] == numbers[8]
-        || numbers[4] == numbers[5]
-        || numbers[4] == numbers[6]
-        || numbers[4] == numbers[7]
-        || numbers[4] == numbers[8]
-        || numbers[5] == numbers[6]
-        || numbers[5] == numbers[7]
-        || numbers[5] == numbers[8]
-        || numbers[6] == numbers[7]
-        || numbers[6] == numbers[8]
-        || numbers[7] == numbers[8])
+fn numbers_are_unique(a: i32, b: i32, c: i32, d: i32, e: i32, f: i32, g: i32, h: i32, i: i32) -> bool {
+    !(a == b
+        || a == c
+        || a == d
+        || a == e
+        || a == f
+        || a == g
+        || a == h
+        || a == i
+        || b == c
+        || b == d
+        || b == e
+        || b == f
+        || b == g
+        || b == h
+        || b == i
+        || c == d
+        || c == e
+        || c == f
+        || c == g
+        || c == h
+        || c == i
+        || d == e
+        || d == f
+        || d == g
+        || d == h
+        || d == i
+        || e == f
+        || e == g
+        || e == h
+        || e == i
+        || f == g
+        || f == h
+        || f == i
+        || g == h
+        || g == i
+        || h == i)
 }
 
-fn sums_are_equal(numbers: [i32; 9]) -> bool {
-    let top_row_sum: i32 = numbers[0] + numbers[1] + numbers[2];
-    let middle_row_sum: i32 = numbers[3] + numbers[4] + numbers[5];
-    let bottom_row_sum: i32 = numbers[6] + numbers[7] + numbers[8];
+fn sums_are_equal(a: i32, b: i32, c: i32, d: i32, e: i32, f: i32, g: i32, h: i32, i: i32) -> bool {
+    let top_row_sum: i32 = a + b + c;
+    let middle_row_sum: i32 = d + e + f;
+    let bottom_row_sum: i32 = g + h + i;
 
     if top_row_sum != middle_row_sum || middle_row_sum != bottom_row_sum {
         return false;
     }
 
-    let left_column_sum: i32 = numbers[0] + numbers[3] + numbers[6];
-    let middle_column_sum: i32 = numbers[1] + numbers[4] + numbers[7];
-    let right_column_sum: i32 = numbers[2] + numbers[5] + numbers[8];
+    let left_column_sum: i32 = a + d + g;
+    let middle_column_sum: i32 = b + e + h;
+    let right_column_sum: i32 = c + f + i;
 
     if bottom_row_sum != left_column_sum || left_column_sum != middle_column_sum || middle_column_sum != right_column_sum {
         return false;
     }
 
-    let nw_se_sum: i32 = numbers[0] + numbers[4] + numbers[8];
-    let sw_ne_sum: i32 = numbers[6] + numbers[4] + numbers[2];
+    let nw_se_sum: i32 = a + e + i;
+    let sw_ne_sum: i32 = g + e + c;
 
     right_column_sum == nw_se_sum && nw_se_sum == sw_ne_sum
 }
@@ -74,8 +74,8 @@ fn main() {
                             (0..LIMIT).for_each(|g| {
                                 (0..LIMIT).for_each(|h| {
                                     (0..LIMIT).for_each(|i| {
-                                        if numbers_are_unique([a, b, c, d, e, f, g, h, i]) {
-                                            if sums_are_equal([a, b, c, d, e, f, g, h, i]) {
+                                        if numbers_are_unique(a, b, c, d, e, f, g, h, i) {
+                                            if sums_are_equal(a, b, c, d, e, f, g, h, i) {
                                                 println!("{:?}", [a, b, c, d, e, f, g, h, i]);
                                             }
                                         }
