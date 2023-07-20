2560772e9385599a98c1f0672d2f73b6dda77bdc
guymarshallpnp <130752424+guymarshallpnp@users.noreply.github.com>
2023-07-20 13:01:05 +0100

    Reimplement rotate() as rotate_square()

--- src/square.rs
+++ src/square.rs
@@ -1,23 +1,23 @@
 struct Square {
-    numbers: [[i32; 3]; 3]
+    numbers: [i32; 9]
 }
 
 impl Square {
-    fn new(numbers: [[i32; 3]; 3]) -> Square {
+    fn new(numbers: [i32; 9]) -> Square {
         Square {numbers}
     }
 
-    fn rotate(&self) {
-        (0..3).iter().for_each(|i| {
-            (i..3).iter().for_each(|j| {
-                let temp = self.numbers[i][j];
-                self.numbers[i][j] = self.numbers[j][i];
-                self.numbers[j][i] = temp;
-            });
-        });
+    fn rotate_square(&self) -> [i32; 9] {
+        let mut rotated_array: [i32; 9] = [0; 9];
 
-        (0..3).iter().for_each(|i| {
-            self.numbers[i].reverse();
-        });
+        rotated_array[0] = self.numbers[2];
+        rotated_array[1] = self.numbers[5];
+        rotated_array[2] = self.numbers[8];
+        rotated_array[3] = self.numbers[1];
+        // rotated_array[4] stays the same (middle)
+        rotated_array[5] = self.numbers[7];
+        rotated_array[6] = self.numbers[0];
+        rotated_array[7] = self.numbers[4];
+        rotated_array[8] = self.numbers[6];
     }
 }
