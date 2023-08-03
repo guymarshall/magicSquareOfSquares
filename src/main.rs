8c1465f7a1ba6fccdc01af3e19d81eb2f398f699
Guy Marshall <guymarshall9@gmail.com>
2023-08-03 22:45:56 +0100

    Drastically improve performance using combinations instead of permutations to get rid of duplicates, then keep as iterator to save RAM

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::collections::HashSet;
 use itertools::Itertools;
-use rayon::prelude::*;
 
 fn generate_square_numbers(count: i32) -> Vec<i32> {
     (0..=count).map(|number| number * number).collect()
@@ -30,15 +28,13 @@ fn sums_are_equal(a: &i32, b: &i32, c: &i32, d: &i32, e: &i32, f: &i32, g: &i32,
 }
 
 fn main() {
-    const LIMIT: i32 = 10;
+    const LIMIT: i32 = 40;
 
     let square_numbers: Vec<i32> = generate_square_numbers(LIMIT);
 
-    let permutations = square_numbers.iter().permutations(9);
+    let permutations = square_numbers.iter().combinations(9);
 
-    let unique_permutations: HashSet<Vec<&i32>> = permutations.collect();
-
-    unique_permutations.par_iter().for_each(|permutation| {
+    permutations.for_each(|permutation| {
         if sums_are_equal(permutation[0], permutation[1], permutation[2], permutation[3], permutation[4], permutation[5], permutation[6], permutation[7], permutation[8]) {
             println!("{:?}", [permutation[0], permutation[1], permutation[2], permutation[3], permutation[4], permutation[5], permutation[6], permutation[7], permutation[8]]);
         }
