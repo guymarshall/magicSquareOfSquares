eb11c6ad9b0d46f097bb9b999a8728373bfd9f05
Guy Marshall <guymarshall9@gmail.com>
2023-08-03 21:00:46 +0100

    Remove duplicates using HashSet and parallelise iterator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::collections::HashSet;
 use itertools::Itertools;
+use rayon::prelude::*;
 
 fn generate_square_numbers(count: i32) -> Vec<i32> {
     (0..=count).map(|number| number * number).collect()
@@ -28,52 +30,17 @@ fn sums_are_equal(a: &i32, b: &i32, c: &i32, d: &i32, e: &i32, f: &i32, g: &i32,
 }
 
 fn main() {
-    const LIMIT: i32 = 80;
+    const LIMIT: i32 = 10;
 
     let square_numbers: Vec<i32> = generate_square_numbers(LIMIT);
 
     let permutations = square_numbers.iter().permutations(9);
 
-    permutations.for_each(|permutation| {
-        if !(permutation[0] == permutation[1]
-            || permutation[0] == permutation[2]
-            || permutation[0] == permutation[3]
-            || permutation[0] == permutation[4]
-            || permutation[0] == permutation[5]
-            || permutation[0] == permutation[6]
-            || permutation[0] == permutation[7]
-            || permutation[0] == permutation[8]
-            || permutation[1] == permutation[2]
-            || permutation[1] == permutation[3]
-            || permutation[1] == permutation[4]
-            || permutation[1] == permutation[5]
-            || permutation[1] == permutation[6]
-            || permutation[1] == permutation[7]
-            || permutation[1] == permutation[8]
-            || permutation[2] == permutation[3]
-            || permutation[2] == permutation[4]
-            || permutation[2] == permutation[5]
-            || permutation[2] == permutation[6]
-            || permutation[2] == permutation[7]
-            || permutation[2] == permutation[8]
-            || permutation[3] == permutation[4]
-            || permutation[3] == permutation[5]
-            || permutation[3] == permutation[6]
-            || permutation[3] == permutation[7]
-            || permutation[3] == permutation[8]
-            || permutation[4] == permutation[5]
-            || permutation[4] == permutation[6]
-            || permutation[4] == permutation[7]
-            || permutation[4] == permutation[8]
-            || permutation[5] == permutation[6]
-            || permutation[5] == permutation[7]
-            || permutation[5] == permutation[8]
-            || permutation[6] == permutation[7]
-            || permutation[6] == permutation[8]
-            || permutation[7] == permutation[8]) {
-            if sums_are_equal(permutation[0], permutation[1], permutation[2], permutation[3], permutation[4], permutation[5], permutation[6], permutation[7], permutation[8]) {
-                println!("{:?}", [permutation[0], permutation[1], permutation[2], permutation[3], permutation[4], permutation[5], permutation[6], permutation[7], permutation[8]]);
-            }
+    let unique_permutations: HashSet<Vec<&i32>> = permutations.collect();
+
+    unique_permutations.par_iter().for_each(|permutation| {
+        if sums_are_equal(permutation[0], permutation[1], permutation[2], permutation[3], permutation[4], permutation[5], permutation[6], permutation[7], permutation[8]) {
+            println!("{:?}", [permutation[0], permutation[1], permutation[2], permutation[3], permutation[4], permutation[5], permutation[6], permutation[7], permutation[8]]);
         }
     });
 }
