9ad1bb56214c5b8ef4a296dafa8f25d86a0fa5b7
guymarshallpnp <130752424+guymarshallpnp@users.noreply.github.com>
2023-08-11 13:19:02 +0100

    Finish main algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,76 @@
 use itertools::Itertools;
+use rayon::prelude::*;
 
 fn generate_square_numbers(count: i32) -> Vec<i32> {
     (0..=count).map(|number| number * number).collect()
 }
 
-fn sums_are_equal(a: &i32, b: &i32, c: &i32, d: &i32, e: &i32, f: &i32, g: &i32, h: &i32, i: &i32) -> bool {
-    let top_row_sum: i32 = a + b + c;
-    let middle_row_sum: i32 = d + e + f;
-    let bottom_row_sum: i32 = g + h + i;
+fn rotate_90_degrees(layout: Vec<i32>) -> Vec<i32> {
+    vec!(
+        layout[6], layout[3], layout[0],
+        layout[7], layout[4], layout[1],
+        layout[8], layout[5], layout[2],
+    )
+}
+
+fn rotate_180_degrees(layout: Vec<i32>) -> Vec<i32> {
+    vec!(
+        layout[8], layout[7], layout[6],
+        layout[5], layout[4], layout[3],
+        layout[2], layout[1], layout[0],
+    )
+}
+
+fn rotate_270_degrees(layout: Vec<i32>) -> Vec<i32> {
+    vec!(
+        layout[2], layout[5], layout[8],
+        layout[1], layout[4], layout[7],
+        layout[0], layout[3], layout[6],
+    )
+}
+
+// fn sums_are_equal(a: &i32, b: &i32, c: &i32, d: &i32, e: &i32, f: &i32, g: &i32, h: &i32, i: &i32) -> bool {
+//     let top_row_sum: i32 = a + b + c;
+//     let middle_row_sum: i32 = d + e + f;
+//     let bottom_row_sum: i32 = g + h + i;
+
+//     if top_row_sum != middle_row_sum || middle_row_sum != bottom_row_sum {
+//         return false;
+//     }
+
+//     let left_column_sum: i32 = a + d + g;
+//     let middle_column_sum: i32 = b + e + h;
+//     let right_column_sum: i32 = c + f + i;
+
+//     if bottom_row_sum != left_column_sum || left_column_sum != middle_column_sum || middle_column_sum != right_column_sum {
+//         return false;
+//     }
+
+//     let nw_se_sum: i32 = a + e + i;
+//     let sw_ne_sum: i32 = g + e + c;
+
+//     right_column_sum == nw_se_sum && nw_se_sum == sw_ne_sum
+// }
+
+fn sums_are_equal(numbers: &Vec<i32>) -> bool {
+    let top_row_sum: i32 = numbers[0] + numbers[1] + numbers[2];
+    let middle_row_sum: i32 = numbers[3] + numbers[4] + numbers[5];
+    let bottom_row_sum: i32 = numbers[6] + numbers[7] + numbers[8];
 
     if top_row_sum != middle_row_sum || middle_row_sum != bottom_row_sum {
         return false;
     }
 
-    let left_column_sum: i32 = a + d + g;
-    let middle_column_sum: i32 = b + e + h;
-    let right_column_sum: i32 = c + f + i;
+    let left_column_sum: i32 = numbers[0] + numbers[3] + numbers[6];
+    let middle_column_sum: i32 = numbers[1] + numbers[4] + numbers[7];
+    let right_column_sum: i32 = numbers[2] + numbers[5] + numbers[8];
 
     if bottom_row_sum != left_column_sum || left_column_sum != middle_column_sum || middle_column_sum != right_column_sum {
         return false;
     }
 
-    let nw_se_sum: i32 = a + e + i;
-    let sw_ne_sum: i32 = g + e + c;
+    let nw_se_sum: i32 = numbers[0] + numbers[4] + numbers[8];
+    let sw_ne_sum: i32 = numbers[6] + numbers[4] + numbers[2];
 
     right_column_sum == nw_se_sum && nw_se_sum == sw_ne_sum
 }
@@ -32,11 +80,21 @@ fn main() {
 
     let square_numbers: Vec<i32> = generate_square_numbers(LIMIT);
 
-    let permutations = square_numbers.iter().combinations(9);
+    // generate 9! permutations
+    // then for every iteration, plug in a different combination of indices
+    // for the squares vector
 
-    permutations.for_each(|permutation| {
-        if sums_are_equal(permutation[0], permutation[1], permutation[2], permutation[3], permutation[4], permutation[5], permutation[6], permutation[7], permutation[8]) {
-            println!("{:?}", [permutation[0], permutation[1], permutation[2], permutation[3], permutation[4], permutation[5], permutation[6], permutation[7], permutation[8]]);
-        }
+    let indices = (0..9).permutations(9);
+
+    let combinations = square_numbers.iter().combinations(9);
+
+    combinations.for_each(|combination| {
+        indices.clone().for_each(|index| {
+            let data: Vec<i32> = index.par_iter().map(|&i| combination[i]).cloned().collect();
+
+            if sums_are_equal(&data) {
+                println!("{:?}", data);
+            }
+        });
     });
 }
