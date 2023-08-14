f6c6212b8fb3d83cbc0527c896682899612b2f21
Guy Marshall <guymarshall9@gmail.com>
2023-08-14 22:24:10 +0100

    Finish second brute force algorithm. Need to optimise using rotations and flips

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use itertools::Itertools;
-use rayon::prelude::*;
 
 fn generate_square_numbers(count: i32) -> Vec<i32> {
     (0..=count).map(|number| number * number).collect()
@@ -29,29 +28,6 @@ fn rotate_270_degrees(layout: Vec<i32>) -> Vec<i32> {
     )
 }
 
-// fn sums_are_equal(a: &i32, b: &i32, c: &i32, d: &i32, e: &i32, f: &i32, g: &i32, h: &i32, i: &i32) -> bool {
-//     let top_row_sum: i32 = a + b + c;
-//     let middle_row_sum: i32 = d + e + f;
-//     let bottom_row_sum: i32 = g + h + i;
-
-//     if top_row_sum != middle_row_sum || middle_row_sum != bottom_row_sum {
-//         return false;
-//     }
-
-//     let left_column_sum: i32 = a + d + g;
-//     let middle_column_sum: i32 = b + e + h;
-//     let right_column_sum: i32 = c + f + i;
-
-//     if bottom_row_sum != left_column_sum || left_column_sum != middle_column_sum || middle_column_sum != right_column_sum {
-//         return false;
-//     }
-
-//     let nw_se_sum: i32 = a + e + i;
-//     let sw_ne_sum: i32 = g + e + c;
-
-//     right_column_sum == nw_se_sum && nw_se_sum == sw_ne_sum
-// }
-
 fn sums_are_equal(numbers: &Vec<i32>) -> bool {
     let top_row_sum: i32 = numbers[0] + numbers[1] + numbers[2];
     let middle_row_sum: i32 = numbers[3] + numbers[4] + numbers[5];
@@ -76,7 +52,7 @@ fn sums_are_equal(numbers: &Vec<i32>) -> bool {
 }
 
 fn main() {
-    const LIMIT: i32 = 40;
+    const LIMIT: i32 = 10;
 
     let square_numbers: Vec<i32> = generate_square_numbers(LIMIT);
 
@@ -90,7 +66,7 @@ fn main() {
 
     combinations.for_each(|combination| {
         indices.clone().for_each(|index| {
-            let data: Vec<i32> = index.par_iter().map(|&i| combination[i]).cloned().collect();
+            let data: Vec<i32> = index.iter().map(|&i| combination[i]).cloned().collect();
 
             if sums_are_equal(&data) {
                 println!("{:?}", data);
