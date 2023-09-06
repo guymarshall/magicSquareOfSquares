cb82ad435ff4439be2b31d8468bad6f37961c6a9
Guy Marshall <guymarshall9@gmail.com>
2023-09-06 22:05:39 +0100

    Remove inline for functions

--- src/sums_are_equal.rs
+++ src/sums_are_equal.rs
@@ -1,4 +1,3 @@
-#[inline(always)]
 pub fn sums_are_equal(numbers: &Vec<&i32>) -> bool {
     let top_row_sum: i32 = numbers[0] + numbers[1] + numbers[2];
     let middle_row_sum: i32 = numbers[3] + numbers[4] + numbers[5];
