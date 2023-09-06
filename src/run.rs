d0c55f9831805b4fbc9d29396921ee87076466e3
Guy Marshall <guymarshall9@gmail.com>
2023-09-06 21:56:47 +0100

    Rename functions for clarity

--- src/run.rs
+++ src/run.rs
@@ -1,7 +1,7 @@
 use rayon::prelude::*;
 use crate::sums_are_equal::sums_are_equal;
 
-pub fn run_with_avx2(combinations: itertools::Combinations<std::slice::Iter<'_, i32>>, indices: Vec<Vec<i32>>) {
+pub fn run_avx2(combinations: itertools::Combinations<std::slice::Iter<'_, i32>>, indices: Vec<Vec<i32>>) {
     combinations.par_bridge().for_each(|combination: Vec<&i32>| {
         indices.iter().for_each(|index| {
             let numbers: Vec<&i32> = index.iter().map(|&i| combination[i as usize]).collect();
@@ -13,7 +13,7 @@ pub fn run_with_avx2(combinations: itertools::Combinations<std::slice::Iter<'_, 
     });
 }
 
-pub fn run_without_avx2(combinations: itertools::Combinations<std::slice::Iter<'_, i32>>, indices: Vec<Vec<i32>>) {
+pub fn run(combinations: itertools::Combinations<std::slice::Iter<'_, i32>>, indices: Vec<Vec<i32>>) {
     combinations.par_bridge().for_each(|combination: Vec<&i32>| {
         indices.iter().for_each(|index| {
             let numbers: Vec<&i32> = index.iter().map(|&i| combination[i as usize]).collect();
