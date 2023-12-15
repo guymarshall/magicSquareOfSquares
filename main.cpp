b6a69af10d4a6a09f719f0c37f682e3654e30d44
Guy Marshall <guymarshall9@gmail.com>
2023-12-15 22:59:31 +0000

    UPDATE: Almost finished conversion to C++

--- main.cpp
+++ main.cpp
@@ -1,75 +1,120 @@
+#include <array>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 bool sumsAreEqual(const std::vector<int32_t>& numbers) {
-    const int32_t top_row_sum = numbers[0] + numbers[1] + numbers[2];
-    const int32_t middle_row_sum = numbers[3] + numbers[4] + numbers[5];
-    const int32_t bottom_row_sum = numbers[6] + numbers[7] + numbers[8];
+    const int32_t topRowSum = numbers[0] + numbers[1] + numbers[2];
+    const int32_t middleRowSum = numbers[3] + numbers[4] + numbers[5];
+    const int32_t bottomRowSum = numbers[6] + numbers[7] + numbers[8];
 
-    if (top_row_sum != middle_row_sum or middle_row_sum != bottom_row_sum) {
+    if (topRowSum != middleRowSum or middleRowSum != bottomRowSum) {
         return false;
     }
 
-    const int32_t left_column_sum = numbers[0] + numbers[3] + numbers[6];
-    const int32_t middle_column_sum = numbers[1] + numbers[4] + numbers[7];
-    const int32_t right_column_sum = numbers[2] + numbers[5] + numbers[8];
+    const int32_t leftColumnSum = numbers[0] + numbers[3] + numbers[6];
+    const int32_t middleColumnSum = numbers[1] + numbers[4] + numbers[7];
+    const int32_t rightColumnSum = numbers[2] + numbers[5] + numbers[8];
 
-    if (bottom_row_sum != left_column_sum or left_column_sum != middle_column_sum or middle_column_sum != right_column_sum) {
+    if (bottomRowSum != leftColumnSum or leftColumnSum != middleColumnSum or middleColumnSum != rightColumnSum) {
         return false;
     }
 
-    const int32_t nw_se_sum = numbers[0] + numbers[4] + numbers[8];
-    const int32_t sw_ne_sum = numbers[6] + numbers[4] + numbers[2];
+    const int32_t nwSeSum = numbers[0] + numbers[4] + numbers[8];
+    const int32_t swNeSum = numbers[6] + numbers[4] + numbers[2];
 
-    return right_column_sum == nw_se_sum and nw_se_sum == sw_ne_sum;
+    return rightColumnSum == nwSeSum and nwSeSum == swNeSum;
 }
 
-// def main():
-//     limit: int = 20
-//
-//     let square_numbers: Vec<int> = (0..=limit).map(|i| i * i).collect();
-//
-//     // generate 9! permutations
-//     // then for every iteration, plug in a different combination of indices
-//     // for the squares vector
-//
-//     let unfiltered_indices: Vec<Vec<int>> = (0..9)
-//         .permutations(9)
-//         .map(|permutation| permutation.into_iter().collect())
-//         .collect();
-//
-//     let permutations_to_ignore: [[int; 9]; 7] = [
-//         // rotation
-//         [6, 3, 0, 7, 4, 1, 8, 5, 2],
-//         [8, 7, 6, 5, 4, 3, 2, 1, 0],
-//         [2, 5, 8, 1, 4, 7, 0, 3, 6],
-//         // flip nw-se
-//         [0, 3, 6, 1, 4, 7, 2, 5, 8],
-//         // flip sw-ne
-//         [8, 5, 2, 7, 4, 1, 6, 3, 0],
-//         // flip n-s
-//         [2, 1, 0, 5, 4, 3, 8, 7, 6],
-//         // flip w-e
-//         [6, 7, 8, 3, 4, 5, 0, 1, 2],
-//     ];
-//
-//     // for every unfiltered_index in unfiltered_indices, remove any permutations that match the permutations_to_ignore array
-//     let indices: Vec<Vec<int>> = unfiltered_indices
-//         .iter()
-//         .filter(|indices| {
-//             permutations_to_ignore.iter().all(|permutation_to_ignore| {
-//                 indices
-//                     .iter()
-//                     .zip(permutation_to_ignore.iter())
-//                     .all(|(index, permutation_index)| index != permutation_index)
-//             })
-//         })
-//         .cloned()
-//         .collect();
-//
-//     let combinations: itertools::Combinations<std::slice::Iter<'_, int>> =
-//         square_numbers.iter().combinations(9);
-//
+std::vector<int32_t> generateSquareNumbers(const int32_t limit) {
+    std::vector<int32_t> squareNumbers(limit + 1);
+    std::iota(squareNumbers.begin(), squareNumbers.end(), 0);
+
+    std::transform(squareNumbers.begin(), squareNumbers.end(), squareNumbers.begin(),
+                   [](const int32_t num) { return num * num; });
+
+    return squareNumbers;
+}
+
+std::vector<std::vector<int32_t>> generateUnfilteredIndices() {
+    std::vector<std::vector<int32_t>> unfilteredIndices;
+
+    std::vector<int32_t> indices(9);
+    std::iota(indices.begin(), indices.end(), 0);
+
+    do {
+        unfilteredIndices.push_back(indices);
+    } while (std::next_permutation(indices.begin(), indices.end()));
+
+    return unfilteredIndices;
+}
+
+bool shouldKeepPermutation(const std::vector<int>& indices, const std::array<std::array<int, 9>, 7>& permutationsToIgnore) {
+    return std::all_of(permutationsToIgnore.begin(), permutationsToIgnore.end(), [&](const std::array<int, 9>& permutationToIgnore) {
+        return std::mismatch(indices.begin(), indices.end(), permutationToIgnore.begin()).first == indices.end();
+    });
+}
+
+std::vector<std::vector<int>> filterIndices(const std::vector<std::vector<int>>& unfilteredIndices, const std::array<std::array<int, 9>, 7>& permutationsToIgnore) {
+    std::vector<std::vector<int>> indices;
+    for (const auto& indices_set : unfilteredIndices) {
+        if (shouldKeepPermutation(indices_set, permutationsToIgnore)) {
+            indices.push_back(indices_set);
+        }
+    }
+    return indices;
+}
+
+std::vector<std::vector<int>> generateCombinations(const std::vector<int>& squareNumbers, const int combinationSize) {
+    std::vector<bool> bitmask(squareNumbers.size(), false);
+    std::fill_n(bitmask.begin(), combinationSize, true);
+
+    std::vector<std::vector<int>> combinations;
+
+    do {
+        std::vector<int> temp;
+        for (size_t i = 0; i < squareNumbers.size(); ++i) {
+            if (bitmask[i]) {
+                temp.push_back(squareNumbers[i]);
+            }
+        }
+        combinations.push_back(temp);
+    } while (std::next_permutation(bitmask.begin(), bitmask.end()));
+
+    return combinations;
+}
+
+int main() {
+    constexpr int32_t limit = 20;
+
+    const std::vector<int32_t> squareNumbers = generateSquareNumbers(limit);
+
+     // generate 9! permutations
+     // then for every iteration, plug in a different combination of indices
+     // for the squares vector
+
+    const std::vector<std::vector<int>> unfilteredIndices = generateUnfilteredIndices();
+
+    const std::array<std::array<int, 9>, 7> permutationsToIgnore = {{
+        // rotation
+        {6, 3, 0, 7, 4, 1, 8, 5, 2},
+        {8, 7, 6, 5, 4, 3, 2, 1, 0},
+        {2, 5, 8, 1, 4, 7, 0, 3, 6},
+        // flip nw-se
+        {0, 3, 6, 1, 4, 7, 2, 5, 8},
+        // flip sw-ne
+        {8, 5, 2, 7, 4, 1, 6, 3, 0},
+        // flip n-s
+        {2, 1, 0, 5, 4, 3, 8, 7, 6},
+        // flip w-e
+        {6, 7, 8, 3, 4, 5, 0, 1, 2},
+    }};
+
+    // for every unfiltered_index in unfiltered_indices, remove any permutations that match the permutations_to_ignore array
+    std::vector<std::vector<int>> indices = filterIndices(unfilteredIndices, permutationsToIgnore);
+
+    std::vector<std::vector<int>> result = generateCombinations(squareNumbers, 9);
+
 //     combinations
 //         .par_bridge()
 //         .for_each(|combination: Vec<&int>| {
@@ -81,8 +126,6 @@ bool sumsAreEqual(const std::vector<int32_t>& numbers) {
 //                 }
 //             });
 //         });
-
-int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
