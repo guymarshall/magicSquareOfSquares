9ed668e2f0fda6ce6b169fd6934b566e078f9afa
Guy Marshall <guy.marshall@pnp.digital>
2024-01-05 13:34:15 +0000

    UPDATE: Drastically improve performance by swapping structs with i32s

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,69 @@
-use std::process;
-use crate::magic_square::MagicSquare;
 use rayon::prelude::*;
 
-mod magic_square;
+fn numbers_are_unique(a: i32, b: i32, c: i32, d: i32, e: i32, f: i32, g: i32, h: i32, i: i32) -> bool {
+    !(a == b
+        || a == c
+        || a == d
+        || a == e
+        || a == f
+        || a == g
+        || a == h
+        || a == i
+        || b == c
+        || b == d
+        || b == e
+        || b == f
+        || b == g
+        || b == h
+        || b == i
+        || c == d
+        || c == e
+        || c == f
+        || c == g
+        || c == h
+        || c == i
+        || d == e
+        || d == f
+        || d == g
+        || d == h
+        || d == i
+        || e == f
+        || e == g
+        || e == h
+        || e == i
+        || f == g
+        || f == h
+        || f == i
+        || g == h
+        || g == i
+        || h == i)
+}
 
-fn numbers_are_unique(numbers: &[u32; 9]) -> bool {
-    !(numbers[0] == numbers[1]
-        || numbers[0] == numbers[2]
-        || numbers[0] == numbers[3]
-        || numbers[0] == numbers[4]
-        || numbers[0] == numbers[5]
-        || numbers[0] == numbers[6]
-        || numbers[0] == numbers[7]
-        || numbers[0] == numbers[8]
-        || numbers[1] == numbers[2]
-        || numbers[1] == numbers[3]
-        || numbers[1] == numbers[4]
-        || numbers[1] == numbers[5]
-        || numbers[1] == numbers[6]
-        || numbers[1] == numbers[7]
-        || numbers[1] == numbers[8]
-        || numbers[2] == numbers[3]
-        || numbers[2] == numbers[4]
-        || numbers[2] == numbers[5]
-        || numbers[2] == numbers[6]
-        || numbers[2] == numbers[7]
-        || numbers[2] == numbers[8]
-        || numbers[3] == numbers[4]
-        || numbers[3] == numbers[5]
-        || numbers[3] == numbers[6]
-        || numbers[3] == numbers[7]
-        || numbers[3] == numbers[8]
-        || numbers[4] == numbers[5]
-        || numbers[4] == numbers[6]
-        || numbers[4] == numbers[7]
-        || numbers[4] == numbers[8]
-        || numbers[5] == numbers[6]
-        || numbers[5] == numbers[7]
-        || numbers[5] == numbers[8]
-        || numbers[6] == numbers[7]
-        || numbers[6] == numbers[8]
-        || numbers[7] == numbers[8])
+fn sums_are_equal(a: i32, b: i32, c: i32, d: i32, e: i32, f: i32, g: i32, h: i32, i: i32) -> bool {
+    let top_row_sum: i32 = a + b + c;
+    let middle_row_sum: i32 = d + e + f;
+    let bottom_row_sum: i32 = g + h + i;
+
+    if top_row_sum != middle_row_sum || middle_row_sum != bottom_row_sum {
+        return false;
+    }
+
+    let left_column_sum: i32 = a + d + g;
+    let middle_column_sum: i32 = b + e + h;
+    let right_column_sum: i32 = c + f + i;
+
+    if bottom_row_sum != left_column_sum || left_column_sum != middle_column_sum || middle_column_sum != right_column_sum {
+        return false;
+    }
+
+    let nw_se_sum: i32 = a + e + i;
+    let sw_ne_sum: i32 = g + e + c;
+
+    right_column_sum == nw_se_sum && nw_se_sum == sw_ne_sum
 }
 
 fn main() {
-    const LIMIT: u32 = 50;
+    const LIMIT: i32 = 60;
 
     (0..LIMIT).for_each(|a| {
         (0..LIMIT).into_par_iter().for_each(|b| {
@@ -55,10 +74,10 @@ fn main() {
                             (0..LIMIT).for_each(|g| {
                                 (0..LIMIT).for_each(|h| {
                                     (0..LIMIT).for_each(|i| {
-                                        let square_of_squares: [u32; 9] = [a, b, c, d, e, f, g, h, i];
-                                        if numbers_are_unique(&square_of_squares) && MagicSquare::new(a, b, c, d, e, f, g, h, i).sums_are_equal() {
-                                            println!("{:?}", MagicSquare::new(a, b, c, d, e, f, g, h, i));
-                                            process::exit(0);
+                                        if numbers_are_unique(a, b, c, d, e, f, g, h, i) {
+                                            if sums_are_equal(a * a, b * b, c * c, d * d, e * e, f * f, g * g, h * h, i * i) {
+                                                println!("{:?}", [a * a, b * b, c * c, d * d, e * e, f * f, g * g, h * h, i * i]);
+                                            }
                                         }
                                     });
                                 });
@@ -68,6 +87,6 @@ fn main() {
                 });
             });
         });
-        println!("{} / {}", a + 1, LIMIT);
+        println!("{} / {}", a, LIMIT);
     });
 }
