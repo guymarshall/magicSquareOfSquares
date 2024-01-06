e2c6dac3dbca8bb9c5057f262857fea8a60b38f0
Guy Marshall <guymarshall9@gmail.com>
2024-01-06 15:52:53 +0000

    UPDATE: Change i32 to u32 as the numbers will never be negative

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::ops::Range;
 use std::process;
 use rayon::prelude::*;
 
-fn numbers_are_unique(a: i32, b: i32, c: i32, d: i32, e: i32, f: i32, g: i32, h: i32, i: i32) -> bool {
+fn numbers_are_unique(a: u32, b: u32, c: u32, d: u32, e: u32, f: u32, g: u32, h: u32, i: u32) -> bool {
     !(a == b
         || a == c
         || a == d
@@ -41,32 +41,32 @@ fn numbers_are_unique(a: i32, b: i32, c: i32, d: i32, e: i32, f: i32, g: i32, h:
         || h == i)
 }
 
-fn sums_are_equal(a: i32, b: i32, c: i32, d: i32, e: i32, f: i32, g: i32, h: i32, i: i32) -> bool {
-    let top_row_sum: i32 = a + b + c;
-    let middle_row_sum: i32 = d + e + f;
-    let bottom_row_sum: i32 = g + h + i;
+fn sums_are_equal(a: u32, b: u32, c: u32, d: u32, e: u32, f: u32, g: u32, h: u32, i: u32) -> bool {
+    let top_row_sum: u32 = a + b + c;
+    let middle_row_sum: u32 = d + e + f;
+    let bottom_row_sum: u32 = g + h + i;
 
     if top_row_sum != middle_row_sum || middle_row_sum != bottom_row_sum {
         return false;
     }
 
-    let left_column_sum: i32 = a + d + g;
-    let middle_column_sum: i32 = b + e + h;
-    let right_column_sum: i32 = c + f + i;
+    let left_column_sum: u32 = a + d + g;
+    let middle_column_sum: u32 = b + e + h;
+    let right_column_sum: u32 = c + f + i;
 
     if bottom_row_sum != left_column_sum || left_column_sum != middle_column_sum || middle_column_sum != right_column_sum {
         return false;
     }
 
-    let nw_se_sum: i32 = a + e + i;
-    let sw_ne_sum: i32 = g + e + c;
+    let nw_se_sum: u32 = a + e + i;
+    let sw_ne_sum: u32 = g + e + c;
 
     right_column_sum == nw_se_sum && nw_se_sum == sw_ne_sum
 }
 
 fn main() {
-    const LIMIT: i32 = 60;
-    const NUMBER_ITERATOR: Range<i32> = 0..LIMIT;
+    const LIMIT: u32 = 60;
+    const NUMBER_ITERATOR: Range<u32> = 0..LIMIT;
 
     NUMBER_ITERATOR.for_each(|a| {
         NUMBER_ITERATOR.into_par_iter().for_each(|b| {
