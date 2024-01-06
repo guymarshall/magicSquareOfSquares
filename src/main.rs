6a78dbab1479390bb7d745fd01320b6ef8e63902
Guy Marshall <guymarshall9@gmail.com>
2024-01-06 16:06:42 +0000

    UPDATE: Increase performance by changing individual u32 variables in functions to a numbers array of u32

--- src/main.rs
+++ src/main.rs
@@ -2,64 +2,64 @@ use std::ops::Range;
 use std::process;
 use rayon::prelude::*;
 
-fn numbers_are_unique(a: u32, b: u32, c: u32, d: u32, e: u32, f: u32, g: u32, h: u32, i: u32) -> bool {
-    !(a == b
-        || a == c
-        || a == d
-        || a == e
-        || a == f
-        || a == g
-        || a == h
-        || a == i
-        || b == c
-        || b == d
-        || b == e
-        || b == f
-        || b == g
-        || b == h
-        || b == i
-        || c == d
-        || c == e
-        || c == f
-        || c == g
-        || c == h
-        || c == i
-        || d == e
-        || d == f
-        || d == g
-        || d == h
-        || d == i
-        || e == f
-        || e == g
-        || e == h
-        || e == i
-        || f == g
-        || f == h
-        || f == i
-        || g == h
-        || g == i
-        || h == i)
+fn numbers_are_unique(numbers: &[u32; 9]) -> bool {
+    !(numbers[0] == numbers[1]
+        || numbers[0] == numbers[2]
+        || numbers[0] == numbers[3]
+        || numbers[0] == numbers[4]
+        || numbers[0] == numbers[5]
+        || numbers[0] == numbers[6]
+        || numbers[0] == numbers[7]
+        || numbers[0] == numbers[8]
+        || numbers[1] == numbers[2]
+        || numbers[1] == numbers[3]
+        || numbers[1] == numbers[4]
+        || numbers[1] == numbers[5]
+        || numbers[1] == numbers[6]
+        || numbers[1] == numbers[7]
+        || numbers[1] == numbers[8]
+        || numbers[2] == numbers[3]
+        || numbers[2] == numbers[4]
+        || numbers[2] == numbers[5]
+        || numbers[2] == numbers[6]
+        || numbers[2] == numbers[7]
+        || numbers[2] == numbers[8]
+        || numbers[3] == numbers[4]
+        || numbers[3] == numbers[5]
+        || numbers[3] == numbers[6]
+        || numbers[3] == numbers[7]
+        || numbers[3] == numbers[8]
+        || numbers[4] == numbers[5]
+        || numbers[4] == numbers[6]
+        || numbers[4] == numbers[7]
+        || numbers[4] == numbers[8]
+        || numbers[5] == numbers[6]
+        || numbers[5] == numbers[7]
+        || numbers[5] == numbers[8]
+        || numbers[6] == numbers[7]
+        || numbers[6] == numbers[8]
+        || numbers[7] == numbers[8])
 }
 
-fn sums_are_equal(a: u32, b: u32, c: u32, d: u32, e: u32, f: u32, g: u32, h: u32, i: u32) -> bool {
-    let top_row_sum: u32 = a + b + c;
-    let middle_row_sum: u32 = d + e + f;
-    let bottom_row_sum: u32 = g + h + i;
+fn sums_are_equal(numbers: &[u32; 9]) -> bool {
+    let top_row_sum: u32 = numbers[0] + numbers[1] + numbers[2];
+    let middle_row_sum: u32 = numbers[3] + numbers[4] + numbers[5];
+    let bottom_row_sum: u32 = numbers[6] + numbers[7] + numbers[8];
 
     if top_row_sum != middle_row_sum || middle_row_sum != bottom_row_sum {
         return false;
     }
 
-    let left_column_sum: u32 = a + d + g;
-    let middle_column_sum: u32 = b + e + h;
-    let right_column_sum: u32 = c + f + i;
+    let left_column_sum: u32 = numbers[0] + numbers[3] + numbers[6];
+    let middle_column_sum: u32 = numbers[1] + numbers[4] + numbers[7];
+    let right_column_sum: u32 = numbers[2] + numbers[5] + numbers[8];
 
     if bottom_row_sum != left_column_sum || left_column_sum != middle_column_sum || middle_column_sum != right_column_sum {
         return false;
     }
 
-    let nw_se_sum: u32 = a + e + i;
-    let sw_ne_sum: u32 = g + e + c;
+    let nw_se_sum: u32 = numbers[0] + numbers[4] + numbers[8];
+    let sw_ne_sum: u32 = numbers[6] + numbers[4] + numbers[2];
 
     right_column_sum == nw_se_sum && nw_se_sum == sw_ne_sum
 }
@@ -77,7 +77,7 @@ fn main() {
                             NUMBER_ITERATOR.for_each(|g| {
                                 NUMBER_ITERATOR.for_each(|h| {
                                     NUMBER_ITERATOR.for_each(|i| {
-                                        if numbers_are_unique(a, b, c, d, e, f, g, h, i) && sums_are_equal(a * a, b * b, c * c, d * d, e * e, f * f, g * g, h * h, i * i) {
+                                        if numbers_are_unique(&[a, b, c, d, e, f, g, h, i]) && sums_are_equal(&[a * a, b * b, c * c, d * d, e * e, f * f, g * g, h * h, i * i]) {
                                             println!("{:?}", [a * a, b * b, c * c, d * d, e * e, f * f, g * g, h * h, i * i]);
                                             process::exit(0);
                                         }
