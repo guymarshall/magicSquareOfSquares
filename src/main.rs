bb720b386ac69e7fa9172cb0619f23009e43f8af
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-01-22 20:47:09 +0000

    UPDATE: Use arrays instead of references to arrays

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rayon::prelude::*;
 use std::io::{stdout, StdoutLock, Write};
 use std::process;
 
-fn numbers_are_unique(numbers: &[&usize; 9]) -> bool {
+fn numbers_are_unique(numbers: [&usize; 9]) -> bool {
     for i in 0..8 {
         for j in (i + 1)..9 {
             if numbers[i] == numbers[j] {
@@ -13,7 +13,7 @@ fn numbers_are_unique(numbers: &[&usize; 9]) -> bool {
     true
 }
 
-fn sums_are_equal(numbers: &[&usize; 9]) -> bool {
+fn sums_are_equal(numbers: [&usize; 9]) -> bool {
     if (numbers[0] + numbers[1] + numbers[2]) != (numbers[3] + numbers[4] + numbers[5])
         || (numbers[3] + numbers[4] + numbers[5]) != (numbers[6] + numbers[7] + numbers[8])
     {
@@ -59,8 +59,8 @@ fn main() {
                             SQUARE_NUMBERS.iter().for_each(|g| {
                                 SQUARE_NUMBERS.iter().for_each(|h| {
                                     SQUARE_NUMBERS.iter().for_each(|i| {
-                                        if numbers_are_unique(&[a, b, c, d, e, f, g, h, i])
-                                            && sums_are_equal(&[a, b, c, d, e, f, g, h, i])
+                                        if numbers_are_unique([a, b, c, d, e, f, g, h, i])
+                                            && sums_are_equal([a, b, c, d, e, f, g, h, i])
                                         {
                                             println!("{:?}", [a, b, c, d, e, f, g, h, i]);
                                             process::exit(0);
