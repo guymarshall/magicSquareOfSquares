36760cf1c61f6017b783e581bc3fa59711132102
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-06-22 16:47:46 +0100

    UPDATE: Remove rayon multithreading - should be 64x slowdown from 21s to 1344s but ended up being only 511s!

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ mod square;
 use std::process;
 use std::time::Instant;
 
-use rayon::prelude::*;
-use crate::square::Square;
+use crate::square::{numbers_are_unique, sums_are_equal};
 
 const fn generate_square_numbers<const COUNT: usize>() -> [usize; COUNT] {
     let mut numbers: [usize; COUNT] = [0usize; COUNT];
@@ -21,20 +20,19 @@ const fn generate_square_numbers<const COUNT: usize>() -> [usize; COUNT] {
 fn main() {
     let start_time: Instant = Instant::now();
 
-    const LIMIT: usize = 200;
+    const LIMIT: usize = 100;
     const SQUARE_NUMBERS: [usize; LIMIT] = generate_square_numbers();
 
     SQUARE_NUMBERS.iter().for_each(|first| {
         SQUARE_NUMBERS.iter().for_each(|second| {
-            SQUARE_NUMBERS.par_iter().for_each(|third| {
+            SQUARE_NUMBERS.iter().for_each(|third| {
                 SQUARE_NUMBERS.iter().for_each(|fourth| {
                     SQUARE_NUMBERS.iter().for_each(|fifth| {
                         SQUARE_NUMBERS.iter().for_each(|sixth| {
                             SQUARE_NUMBERS.iter().for_each(|seventh| {
                                 SQUARE_NUMBERS.iter().for_each(|eighth| {
                                     SQUARE_NUMBERS.iter().for_each(|ninth| {
-                                        let square: Square = Square {first, second, third, fourth, fifth, sixth, seventh, eighth, ninth};
-                                        if square.sums_are_equal() && square.numbers_are_unique() {
+                                        if sums_are_equal([*first, *second, *third, *fourth, *fifth, *sixth, *seventh, *eighth, *ninth]) && numbers_are_unique([*first, *second, *third, *fourth, *fifth, *sixth, *seventh, *eighth, *ninth]) {
                                             println!("{:?}", [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth]);
                                             process::exit(0);
                                         }
--- src/square.rs
+++ src/square.rs
@@ -1,70 +1,56 @@
-pub struct Square<'a> {
-    pub first: &'a usize,
-    pub second: &'a usize,
-    pub third: &'a usize,
-    pub fourth: &'a usize,
-    pub fifth: &'a usize,
-    pub sixth: &'a usize,
-    pub seventh: &'a usize,
-    pub eighth: &'a usize,
-    pub ninth: &'a usize,
+pub(crate) fn numbers_are_unique(numbers: [usize; 9]) -> bool {
+    numbers[0] != numbers[1] &&
+        numbers[0] != numbers[2] &&
+        numbers[0] != numbers[3] &&
+        numbers[0] != numbers[4] &&
+        numbers[0] != numbers[5] &&
+        numbers[0] != numbers[6] &&
+        numbers[0] != numbers[7] &&
+        numbers[0] != numbers[8] &&
+        numbers[1] != numbers[2] &&
+        numbers[1] != numbers[3] &&
+        numbers[1] != numbers[4] &&
+        numbers[1] != numbers[5] &&
+        numbers[1] != numbers[6] &&
+        numbers[1] != numbers[7] &&
+        numbers[1] != numbers[8] &&
+        numbers[2] != numbers[3] &&
+        numbers[2] != numbers[4] &&
+        numbers[2] != numbers[5] &&
+        numbers[2] != numbers[6] &&
+        numbers[2] != numbers[7] &&
+        numbers[2] != numbers[8] &&
+        numbers[3] != numbers[4] &&
+        numbers[3] != numbers[5] &&
+        numbers[3] != numbers[6] &&
+        numbers[3] != numbers[7] &&
+        numbers[3] != numbers[8] &&
+        numbers[4] != numbers[5] &&
+        numbers[4] != numbers[6] &&
+        numbers[4] != numbers[7] &&
+        numbers[4] != numbers[8] &&
+        numbers[5] != numbers[6] &&
+        numbers[5] != numbers[7] &&
+        numbers[5] != numbers[8] &&
+        numbers[6] != numbers[7] &&
+        numbers[6] != numbers[8] &&
+        numbers[7] != numbers[8]
 }
 
-impl<'a> Square<'a> {
-    pub(crate) fn numbers_are_unique(&self) -> bool {
-        self.first != self.second &&
-            self.first != self.third &&
-            self.first != self.fourth &&
-            self.first != self.fifth &&
-            self.first != self.sixth &&
-            self.first != self.seventh &&
-            self.first != self.eighth &&
-            self.first != self.ninth &&
-            self.second != self.third &&
-            self.second != self.fourth &&
-            self.second != self.fifth &&
-            self.second != self.sixth &&
-            self.second != self.seventh &&
-            self.second != self.eighth &&
-            self.second != self.ninth &&
-            self.third != self.fourth &&
-            self.third != self.fifth &&
-            self.third != self.sixth &&
-            self.third != self.seventh &&
-            self.third != self.eighth &&
-            self.third != self.ninth &&
-            self.fourth != self.fifth &&
-            self.fourth != self.sixth &&
-            self.fourth != self.seventh &&
-            self.fourth != self.eighth &&
-            self.fourth != self.ninth &&
-            self.fifth != self.sixth &&
-            self.fifth != self.seventh &&
-            self.fifth != self.eighth &&
-            self.fifth != self.ninth &&
-            self.sixth != self.seventh &&
-            self.sixth != self.eighth &&
-            self.sixth != self.ninth &&
-            self.seventh != self.eighth &&
-            self.seventh != self.ninth &&
-            self.eighth != self.ninth
+pub(crate) fn sums_are_equal(numbers: [usize; 9]) -> bool {
+    if (numbers[0] + numbers[1] + numbers[2]) != (numbers[3] + numbers[4] + numbers[5])
+        || (numbers[3] + numbers[4] + numbers[5]) != (numbers[6] + numbers[7] + numbers[8])
+    {
+        return false;
     }
 
-    pub(crate) fn sums_are_equal(&self) -> bool {
-        if (self.first + self.second + self.third) != (self.fourth + self.fifth + self.sixth)
-            || (self.fourth + self.fifth + self.sixth) != (self.seventh + self.eighth + self.ninth)
-        {
-            return false;
-        }
-
-        if (self.seventh + self.eighth + self.ninth) != (self.first + self.fourth + self.seventh)
-            || (self.first + self.fourth + self.seventh) != (self.second + self.fifth + self.eighth)
-            || (self.second + self.fifth + self.eighth) != (self.third + self.sixth + self.ninth)
-        {
-            return false;
-        }
-
-        (self.third + self.sixth + self.ninth) == (self.first + self.fifth + self.ninth)
-            && (self.first + self.fifth + self.ninth) == (self.seventh + self.fifth + self.third)
+    if (numbers[6] + numbers[7] + numbers[8]) != (numbers[0] + numbers[3] + numbers[6])
+        || (numbers[0] + numbers[3] + numbers[6]) != (numbers[1] + numbers[4] + numbers[7])
+        || (numbers[1] + numbers[4] + numbers[7]) != (numbers[2] + numbers[5] + numbers[8])
+    {
+        return false;
     }
+
+    (numbers[2] + numbers[5] + numbers[8]) == (numbers[0] + numbers[4] + numbers[8])
+        && (numbers[0] + numbers[4] + numbers[8]) == (numbers[6] + numbers[4] + numbers[2])
 }
