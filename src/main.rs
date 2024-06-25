07369cdc71f60b81fa20e0200d1e2f7b25372878
Guy Marshall <guymarshall9@gmail.com>
2024-06-25 19:04:06 +0100

    UPDATE: Somehow increase performance by making square numbers not const

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::time::Instant;
 
 use crate::square::{numbers_are_unique, sums_are_equal};
 
-const fn generate_square_numbers<const COUNT: usize>() -> [usize; COUNT] {
+fn generate_square_numbers<const COUNT: usize>() -> [usize; COUNT] {
     let mut numbers: [usize; COUNT] = [0usize; COUNT];
 
     let mut counter: usize = 0;
@@ -20,18 +20,18 @@ const fn generate_square_numbers<const COUNT: usize>() -> [usize; COUNT] {
 fn main() {
     let start_time: Instant = Instant::now();
 
-    const LIMIT: usize = 100;
-    const SQUARE_NUMBERS: [usize; LIMIT] = generate_square_numbers();
+    const LIMIT: usize = 50;
+    let square_numbers: [usize; LIMIT] = generate_square_numbers();
 
-    SQUARE_NUMBERS.iter().for_each(|first| {
-        SQUARE_NUMBERS.iter().for_each(|second| {
-            SQUARE_NUMBERS.iter().for_each(|third| {
-                SQUARE_NUMBERS.iter().for_each(|fourth| {
-                    SQUARE_NUMBERS.iter().for_each(|fifth| {
-                        SQUARE_NUMBERS.iter().for_each(|sixth| {
-                            SQUARE_NUMBERS.iter().for_each(|seventh| {
-                                SQUARE_NUMBERS.iter().for_each(|eighth| {
-                                    SQUARE_NUMBERS.iter().for_each(|ninth| {
+    square_numbers.iter().for_each(|first| {
+        square_numbers.iter().for_each(|second| {
+            square_numbers.iter().for_each(|third| {
+                square_numbers.iter().for_each(|fourth| {
+                    square_numbers.iter().for_each(|fifth| {
+                        square_numbers.iter().for_each(|sixth| {
+                            square_numbers.iter().for_each(|seventh| {
+                                square_numbers.iter().for_each(|eighth| {
+                                    square_numbers.iter().for_each(|ninth| {
                                         if sums_are_equal([*first, *second, *third, *fourth, *fifth, *sixth, *seventh, *eighth, *ninth]) && numbers_are_unique([*first, *second, *third, *fourth, *fifth, *sixth, *seventh, *eighth, *ninth]) {
                                             println!("{:?}", [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth]);
                                             process::exit(0);
